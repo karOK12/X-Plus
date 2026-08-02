b72b3abc32700f001a49abc80737625e1cef60d5
karOK12 <Hfsmrkarar1993@gmail.com>
2026-08-02 22:18:25 +0300

    Fix Vercel API

--- api/index.js
+++ api/index.js
@@ -1,20 +1,3 @@
-require("dotenv").config();
-
-const express = require("express");
-const app = express();
-
-app.use(express.json());
-
-const authRoutes = require("./routes/auth");
-
-app.use("/api/auth", authRoutes);
+const app = require("../server/server");
 
 module.exports = app;
-
-if (require.main === module) {
-  const PORT = process.env.PORT || 3000;
-
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
